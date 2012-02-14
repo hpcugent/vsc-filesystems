2ab88a11e6028bdc5b3e7221615da4319cbe99e9
Wouter Depypere <wouter.depypere@ugent.be>
2012-02-14 15:26:25 +0100

    moved logging to appropriate package

--- vsc/__init__.py
+++
@@ -1,4 +0,0 @@
-#the vsc namespace is used in different folders allong the system
-#so explicitly declare this is also the vsc namespace
-import pkg_resources
-pkg_resources.declare_namespace(__name__)
