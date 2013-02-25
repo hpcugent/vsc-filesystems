2ee9face0743e1b20b6b30c685e8f7b030b98efa
Andy Georges <itkovian@gmail.com>
2013-02-25 14:18:40 +0100

    Changed the license header for the setup script.

--- setup.py
+++ setup.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: latin-1 -*-
-#
+##
 # Copyright 2009-2013 Ghent University
 #
-# This file is part of vsc-base,
+# This file is part of vsc-filesystems,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
 # with support of Ghent University (http://ugent.be/hpc),
 # the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
 # the Hercules foundation (http://www.herculesstichting.be/in_English)
 # and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
 #
-# http://github.com/hpcugent/vsc-base
+# All rights reserved.
 #
-# vsc-base is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Library General Public License as
-# published by the Free Software Foundation, either version 2 of
-# the License, or (at your option) any later version.
-#
-# vsc-base is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Library General Public License for more details.
-#
-# You should have received a copy of the GNU Library General Public License
-# along with vsc-base. If not, see <http://www.gnu.org/licenses/>.
-#
+##
 """
-vsc-base base distribution setup.py
+vsc-filesystems base distribution setup.py
 
 @author: Stijn De Weirdt (Ghent University)
 @author: Andy Georges (Ghent University)
