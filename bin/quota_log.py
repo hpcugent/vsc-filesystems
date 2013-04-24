6011d9909a1eba9f3f2c7dc148b8fdc36ad496ff
Andy Georges <itkovian@gmail.com>
2013-04-24 17:20:35 +0200

    quota loggins, storing data in gzipped json format

--- bin/quota_log.py
+++ bin/quota_log.py
@@ -14,11 +14,13 @@ in a zip file, named by date and filesystem.
 @author Andy Georges
 """
 
-
+import gzip
+import json
 import os
 import sys
 import time
 
+from vsc.filesystems.gpfs import GpfsOperations
 from vsc.utils import fancylogger
 from vsc.utils.availability import proceed_on_ha_service
 from vsc.utils.generaloption import simple_option
@@ -26,8 +28,96 @@ from vsc.utils.lock import lock_or_bork, release_or_bork
 from vsc.utils.nagios import NagiosReporter, NagiosResult, NAGIOS_EXIT_OK, NAGIOS_EXIT_WARNING
 from vsc.utils.timestamp_pid_lockfile import TimestampedPidLockfile
 
+#Constants
+NAGIOS_CHECK_FILENAME = '/var/log/pickles/quota_log.nagios.pickle'
+NAGIOS_HEADER = 'quota_log'
+NAGIOS_CHECK_INTERVAL_THRESHOLD = (6 * 60 + 5) * 60   # 365 minutes -- little over 6 hours.
+
+QUOTA_LOG_LOCK_FILE = '/var/run/dshowq_tpid.lock'
+QUOTA_LOG_ZIP_PATH = '/var/log/quota/zips'
+
+logger = fancylogger.getLogger(__name__)
+fancylogger.logToScreen(True)
+fancylogger.setLogLevelInfo()
 
 
+def main():
+    # Collect all info
+
+    # Note: debug option is provided by generaloption
+    # Note: other settings, e.g., ofr each cluster will be obtained from the configuration file
+    options = {
+        'nagios': ('print out nagion information', None, 'store_true', False, 'n'),
+        'nagios_check_filename': ('filename of where the nagios check data is stored', str, 'store', NAGIOS_CHECK_FILENAME),
+        'nagios_check_interval_threshold': ('threshold of nagios checks timing out', None, 'store', NAGIOS_CHECK_INTERVAL_THRESHOLD),
+        'location': ('path to store the gzipped files', None, 'store', QUOTA_LOG_ZIP_PATH),
+        'ha': ('high-availability master IP address', None, 'store', None),
+        'dry-run': ('do not make any updates whatsoever', None, 'store_true', False),
+    }
+
+    opts = simple_option(options)
+
+    nagios_reporter = NagiosReporter(NAGIOS_HEADER, NAGIOS_CHECK_FILENAME, NAGIOS_CHECK_INTERVAL_THRESHOLD)
+    if opts.options.nagios:
+        logger.debug("Producing Nagios report and exiting.")
+        nagios_reporter.report_and_exit()
+        sys.exit(0)  # not reached
+
+    if not proceed_on_ha_service(opts.options.ha):
+        logger.warning("Not running on the target host in the HA setup. Stopping.")
+        nagios_reporter.cache(NAGIOS_EXIT_WARNING,
+                        NagiosResult("Not running on the HA master."))
+        sys.exit(NAGIOS_EXIT_WARNING)
+
+    lockfile = TimestampedPidLockfile(DSHOWQ_LOCK_FILE)
+    lock_or_bork(lockfile, nagios_reporter)
+
+    logger.info("starting dshowq run")
+
+    filesystem_error = 0
+    filesystem_ok = 0
+    error = False
+
+    try:
+        gpfs = GpfsOperations()
+        quota = gpfs.list_quota()
+
+        for key in quota:
+            try:
+                filename = "gpfs_quota_%s_%s.gz" % (time.strftime("%Y%m%d-%H:%M"), key)
+                path = os.path.join(opt.options.location, filename)
+                zipfile = gzip.open(path, 'wb', 9)  # Compress to the max
+                zipfile.write(json.dumps(quota[key]))
+                zipfile.close()
+                filesystem_ok += 1
+                logger.info("Stored quota information for FS %s" % (key))
+            except Exception, err:
+                logger.exception("Failed storing quota information for FS %s" % (key))
+                filesystem_error += 1
+    except Exception, err:
+        error = True
+
+
+    logger.info("Finished dshowq")
+
+    #FIXME: this still looks fugly
+    bork_result = NagiosResult("lock release failed",
+                               fs=filesystems_ok,
+                               fs_error=filesystems_error)
+    release_or_bork(lockfile, nagios_reporter, bork_result)
+
+    if error:
+        nagios_reporter.cache(NAGIOS_EXIT_OK,
+                              NagiosResult("quota logged",
+                                           fs=filesystems_ok,
+                                           fs_error=filesystems_error))
+    else:
+        nagios_reporter.cache(NAGIOS_EXIT_CRITICAL,
+                              NagiosResult("quota not obtained",
+                                           fs=0,
+                                           fs_error=0))
+
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
