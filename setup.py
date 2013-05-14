d34a447644c0093d6df47d7af76c9a433da54780
Andy Georges <itkovian@gmail.com>
2013-05-14 11:45:19 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ shared_setup.SHARED_TARGET.update({
 
 PACKAGE = {
     'name': 'vsc-filesystems',
-    'version': '0.6',
+    'version': '0.7',
     'author': [sdw, ag],
     'maintainer': [sdw, ag],
     'packages': ['vsc', 'vsc.filesystem'],
