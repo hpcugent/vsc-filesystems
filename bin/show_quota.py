0ab0d5e993b3a2af1a36852e1af2db66319cd9cf
Andy Georges <itkovian@gmail.com>
2013-11-28 10:48:10 +0100

    Split up the printing into two functions: one for user, one for VO

--- bin/show_quota.py
+++ bin/show_quota.py
@@ -27,7 +27,7 @@ import os
 import time
 from pwd import getpwuid
 
-from vsc.config.base import VscStorage
+from vsc.config.base import VSC, VscStorage
 from vsc.filesystem.quota.entities import QuotaUser, QuotaFileset
 from vsc.utils import fancylogger
 from vsc.utils.cache import FileCache
@@ -69,26 +69,10 @@ def quota_pretty_print(storage_name, fileset, quota_information, fileset_prefixe
     return s
 
 
-def main():
-
-    options = {
-        'storage': ('the VSC filesystems that are checked by this script', 'strlist', 'store', []),
-        'threshold': ('allowed the time difference between the cached quota and the time of running', None, 'store',
-                      DEFAULT_ALLOWED_TIME_THRESHOLD),
-        'fileset_prefixes': ('the filesets that we allow for showing QuotaUser', 'strlist', 'store', []),
-        'vo': ('provide storage details for the VO you belong to', None, 'store_true', False)
-    }
-    opts = simple_option(options, config_files=['/etc/quota_information.conf'])
-
-    storage = VscStorage()
-    user_name = getpwuid(os.getuid())[0]
-
-    vos = [g.gr_name for g in grp.getgrall() if user_name in g.gr_mem and g.gr_name.startswith('gvo')]
-
-    opts.options.vo = opts.options.vo and vos
-
-    now = time.time()
-
+def print_user_quota(opts, storage, user_name, now):
+    """
+    Print the quota for the user, i.e., USR quota in all filesets the user hass access to.
+    """
     print "User quota:"
     for storage_name in opts.options.storage:
 
@@ -99,7 +83,7 @@ def main():
         cache = FileCache(path)
         try:
             (timestamp, quota) = cache.load('quota')
-        except TypeError, err:
+        except TypeError:
             logger.debug("Cannot load data from %s" % (path,))
             print "%s: WARNING: No quota information found" % (storage_name,)
             continue
@@ -113,31 +97,65 @@ def main():
                 if pp:
                     print pp
 
+
+def print_vo_quota(opts, storage, vos, now):
+    """
+    Print the quota for the VO fileset.
+    """
+    print "\nVO quota:"
+    for storage_name in [s for s in opts.options.storage if s != 'VSC_HOME']:  # No VOs on VSC_HOME atm
+
+        mount_point = storage[storage_name].login_mount_point
+        path_template = storage.path_templates[storage_name]['vo']
+        path = os.path.join(mount_point, path_template[0], path_template[1](vos[0]), ".quota_fileset.json.gz")
+
+        cache = FileCache(path)
+        try:
+            (timestamp, quota) = cache.load('quota')
+        except TypeError:
+            logger.debug("Cannot load data from %s" % (path,))
+            print "%s: WARNING: No VO quota information found" % (storage_name,)
+            continue
+
+        if now - timestamp > opts.options.threshold:
+            print "%s: WARNING: no recent VO quota information (age of data is %d minutes)" % (storage_name,
+                                                                                                (now-timestamp)/60)
+        else:
+            for (fileset, qi) in quota.quota_map.items():
+                pp = quota_pretty_print(storage_name, fileset, qi, opts.options.fileset_prefixes)
+                if pp:
+                    print pp
+
+
+
+def main():
+
+    options = {
+        'storage': ('the VSC filesystems that are checked by this script', 'strlist', 'store', []),
+        'threshold': ('allowed the time difference between the cached quota and the time of running', None, 'store',
+                      DEFAULT_ALLOWED_TIME_THRESHOLD),
+        'fileset_prefixes': ('the filesets that we allow for showing QuotaUser', 'strlist', 'store', []),
+        'vo': ('provide storage details for the VO you belong to', None, 'store_true', False)
+    }
+    opts = simple_option(options, config_files=['/etc/quota_information.conf'])
+
+    storage = VscStorage()
+    vsc = VSC(False)
+    user_name = getpwuid(os.getuid())[0]
+
+    vos = [g.gr_name for g in grp.getgrall()
+                     if user_name in g.gr_mem
+                     and g.gr_name.startswith('gvo')
+                     and g.gr_name != vsc.default_vo]  # default VO has no quota associated with it
+
+    opts.options.vo = opts.options.vo and vos
+
+    now = time.time()
+
+    print_user_quota(opts, storage, user_name, now)
+
     if opts.options.vo:
-
-        print "\nVO quota:"
-        for storage_name in [s for s in opts.options.storage if s != 'VSC_HOME']:  # No VOs on VSC_HOME atm
-
-            mount_point = storage[storage_name].login_mount_point
-            path_template = storage.path_templates[storage_name]['vo']
-            path = os.path.join(mount_point, path_template[0], path_template[1](vos[0]), ".quota_fileset.json.gz")
-
-            cache = FileCache(path)
-            try:
-                (timestamp, quota) = cache.load('quota')
-            except TypeError, err:
-                logger.debug("Cannot load data from %s" % (path,))
-                print "%s: WARNING: No VO quota information found" % (storage_name,)
-                continue
-
-            if now - timestamp > opts.options.threshold:
-                print "%s: WARNING: no recent VO quota information (age of data is %d minutes)" % (storage_name,
-                                                                                                   (now-timestamp)/60)
-            else:
-                for (fileset, qi) in quota.quota_map.items():
-                    pp = quota_pretty_print(storage_name, fileset, qi, opts.options.fileset_prefixes)
-                    if pp:
-                        print pp
+        print_vo_quota(opts, storage, vos, now)
 
 
 if __name__ == '__main__':
