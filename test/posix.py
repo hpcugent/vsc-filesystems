5220a7038ff9d54c2d3be7c6c0e7288ffda008c6
Jens Timmerman <jens.timmerman@ugent.be>
2016-05-03 17:20:49 +0200

    prepare for new vsc_install

--- test/posix.py
+++ test/posix.py
@@ -1,16 +1,17 @@
-##
-# Copyright 2014-2015 Ghent University
+#
+# Copyright 2014-2016 Ghent University
 #
 # This file is part of vsc-filesystems,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
 # with support of Ghent University (http://ugent.be/hpc),
 # the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
-# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
 # and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/hpcugent/vsc-filesystems
 #
 # All rights reserved.
 #
-##
 """
 Unit tests for vsc.filesystems.posix
 
