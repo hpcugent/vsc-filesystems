0847cbefd3309dfb57389179f0c82bdbb5a405f9
Ward Poelmans <wpoely86@gmail.com>
2020-03-30 14:35:46 +0200

    drop ifilter and use list comprension

--- lib/vsc/filesystem/gpfs.py
+++ lib/vsc/filesystem/gpfs.py
@@ -29,7 +29,7 @@ import re
 from collections import namedtuple
 from urllib import unquote as percentdecode
 from socket import gethostname
-from itertools import dropwhile, ifilter
+from itertools import dropwhile
 
 from vsc.config.base import GPFS_DEFAULT_INODE_LIMIT
 from vsc.filesystem.posix import PosixOperations, PosixOperationError
@@ -282,8 +282,7 @@ class GpfsOperations(with_metaclass(Singleton, PosixOperations)):
             rest = {}
             for typ in ('State', 'Event'):
                 try:
-                    filt = ifilter(lambda x: x[1] == typ, what)
-                    fields = [(len(x), x) for x in filt]
+                    fields = [(len(x), x) for x in what if x[1] == typ]
                 except IndexError:
                     self.log.raiseException("No valid lines for output: %s" % (out), GpfsOperationError)
                 if len(fields):
