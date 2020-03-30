28157f3459160b0f026292c587935964c9f86a7a
Ward Poelmans <wpoely86@gmail.com>
2020-03-30 16:36:36 +0200

    bump version more

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from vsc.install.shared_setup import ag, kh, sdw, kw, wdp
 
 
 PACKAGE = {
-    'version': '0.50.0',
+    'version': '1.0.0',
     'author': [sdw, ag, kh],
     'maintainer': [sdw, ag, kh, kw, wdp],
     'setup_requires': ['vsc-install >= 0.15.2'],
