9514f8b03649994bb6eba3d39b799fa06f46bf46
Andy Georges <itkovian@gmail.com>
2020-09-14 13:42:51 +0200

    bump: version to 1.1.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from vsc.install.shared_setup import ag, kh, sdw, kw, wdp
 
 
 PACKAGE = {
-    'version': '1.1.0',
+    'version': '1.1.1',
     'author': [sdw, ag, kh],
     'maintainer': [sdw, ag, kh, kw, wdp],
     'setup_requires': ['vsc-install >= 0.15.2'],
