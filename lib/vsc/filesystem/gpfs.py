67db221e3b112824d33e659969f84edeadc16d05
Kenneth Waegeman <kenneth.waegeman@ugent.be>
2022-01-20 14:46:03 +0100

    happy new year

--- lib/vsc/filesystem/gpfs.py
+++ lib/vsc/filesystem/gpfs.py
@@ -1,6 +1,6 @@
 # -*- coding: latin-1 -*-
 #
-# Copyright 2009-2021 Ghent University
+# Copyright 2009-2022 Ghent University
 #
 # This file is part of vsc-filesystems,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
