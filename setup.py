d4763bf4898d3bfadc33eaad837e51e5841c9238
Kenneth Waegeman <kenneth.waegeman@ugent.be>
2022-03-16 11:44:54 +0100

    bumpe

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ else:
     install_requires.append('pyyaml')
 
 PACKAGE = {
-    'version': '1.2.18',
+    'version': '1.2.19',
     'author': [sdw, ag, kh, kw],
     'maintainer': [sdw, ag, kh, kw, wdp],
     'setup_requires': ['vsc-install >= 0.15.2'],
